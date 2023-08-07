094f7a045c4940bde6201f959de81bcc40a1e9cf
Student <Student@PC509>
2023-08-07 16:43:45 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         task1();
         task2();
+
     }
         static void task1(){
         try {
